1e3e4aadb3e77b10f821ab5dbb7d1d0fef750309
Abidemi Avoseh <105747793+AbidemiDataDriven@users.noreply.github.com>
2024-09-17 13:15:24 +0100

    Add files via upload

---
+++ Employee_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+st.title("Salary Estimation Application")
+
+st.divider()
+
+years_at_company =st.number_input("Enter years at Company", min_value = 0, max_value = 20)
+satisfaction_level =st.number_input("Sastifaction Level", min_value = 0)
+average_monthly_hours = st.number_input("Average Monthly Hours", min_value=120, max_value=400)
+
+X = [years_at_company, satisfaction_level, average_monthly_hours] 
+
+scaler =joblib.load("scaler.pkl")
+model = joblib.load("model.pkl")
+
+predict_button = st.button("Press for predicting the Salary")
+
+st.divider()
+
+if predict_button:
+    
+    st.balloons()
+    
+    X1 = np.array(X)
+    
+    X_array =scaler.transform([X1])
+    
+    prediction = model.predict(X_array)[0]
+    
+    st.write(f"Salary prediction is {prediction}")
+    
+    
+    
+    
+else:
+    st.write("please enter the values and press to the predict button")
+        
+    
